9106d4fd1339be4d1784c8eb151b74a6f3a5d8b6
oGabri-el <gabrieldlbrj@gmail.com>
2022-05-14 21:38:27 -0300

    first commit

---
+++ src/br/com/dio/primeiroPrograma.java
@@ -0,0 +1,7 @@
+package br.com.dio;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        System.out.println("Hello Everyone! Remember that 2 + 2 is " + (2+2) + '!');
+    }
+}
